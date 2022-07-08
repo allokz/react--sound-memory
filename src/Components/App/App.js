69a2a5b1e730b66139afc6e4f0476f5a792a2c5d
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-08 16:51:05 +0200

    Include PlayingField in App and add generateCards function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,32 @@
-import logo from '../../logo.svg';
 import './App.css';
 import newGameIcon from './new-game.png';
+import { PlayingField } from '../PlayingField/PlayingField';
+
 
 function App() {
-  return (
-    <div className="App">
+
+    const generateCards = numberOfCards => {
+        const cards = [];
+        for (let i = 0; i < numberOfCards; i++) {
+            cards.push({id: i});
+        }
+        return cards;
+    }
+
+    return (
+        <div className="App">
+            <header>
                 <h1>Sound Memory</h1>
                 <button>
                     <img src={newGameIcon} alt='Reset icon' />
                     New Game
                 </button>
-      </header>
-    </div>
-  );
+            </header>
+            <main>
+                <PlayingField cards={generateCards(20)} />
+            </main>
+        </div>
+    );
 }
 
 export default App;

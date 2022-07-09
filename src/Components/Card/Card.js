627722c7525df440252c0fce6666fab4cda3fac6
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-09 17:52:51 +0200

    Added effect to toggle image visibility

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Card.css';
 import { images } from './images/index';
 
 
 export function Card(props) {
+    const [visibility, setVisibility] = useState('hidden');
+
+    // switch visibility of image and the cursor style
+    useEffect(() => {
+        const card = document.getElementById(props.card.id);
+
+        card.firstElementChild.style.visibility = visibility;
+
+        if (visibility === 'visible') {
+            card.style.cursor = 'default';
+        } else {
+            card.style.cursor = 'pointer';
+        }
+
+        // console.log('visibility of ' + props.card.id + ' = ' + visibility);
+    }, [visibility]);
+
     const handleClick = () => {
         if (props.numberOfOpenCards < 2 && visibility === 'hidden') {
             setVisibility('visible');

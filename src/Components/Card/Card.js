e5e4a12ca52f128f4319315e1c6e712cdae3e0a5
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-09 17:52:07 +0200

    Add onClick attribut and handler function to Card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,16 @@ import { images } from './images/index';
 
 
 export function Card(props) {
+    const handleClick = () => {
+        if (props.numberOfOpenCards < 2 && visibility === 'hidden') {
+            setVisibility('visible');
+            props.incrementOpenCards(props.card.id);
+        }
+    }
+
     return (
-        <div className='card' id={props.card.id} ></div>
+        <div className='card' id={props.card.id} onClick={handleClick} >
+             <img src={images[props.card.image]} />
+        </div>
     )
 }

247c847e5f212b2efe7eb71cad534378ef148751
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-09 20:33:51 +0200

    Add shuffle function to randomize cards on deck

--- src/Components/MemoryImages/MemoryImages.js
+++ src/Components/MemoryImages/MemoryImages.js
@@ -4,4 +4,12 @@ import { Card } from '../Card/Card';
 
 export function MemoryImages(props) {
 
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+            [array[i], array[j]] = [array[j], array[i]]; // the same as: let temp = array[i]; array[i] = array[j]; array[j] = temp;
+        }
+        return array;
+    }
+    
 }

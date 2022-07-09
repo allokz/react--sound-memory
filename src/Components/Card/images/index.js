e5707037612b8f4eb0511930c3c00d8a45651529
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-09 17:47:11 +0200

    Add card background images

---
+++ src/Components/Card/images/index.js
@@ -0,0 +1,23 @@
+import cardBg1 from './card-bg-1.jpg';
+import cardBg2 from './card-bg-2.jpg';
+import cardBg3 from './card-bg-3.jpg';
+import cardBg4 from './card-bg-4.jpg';
+import cardBg5 from './card-bg-5.jpg';
+import cardBg6 from './card-bg-6.jpg';
+import cardBg7 from './card-bg-7.jpg';
+import cardBg8 from './card-bg-8.jpg';
+import cardBg9 from './card-bg-9.jpg';
+import cardBg10 from './card-bg-10.jpg';
+
+export const images = [
+    cardBg1,
+    cardBg2,
+    cardBg3,
+    cardBg4,
+    cardBg5,
+    cardBg6,
+    cardBg7,
+    cardBg8,
+    cardBg9,
+    cardBg10
+];

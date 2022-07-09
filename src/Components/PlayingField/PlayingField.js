5f1d3579c7571077ca12966bd3156b43312aee4c
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-09 18:22:25 +0200

    Rearranged playing field section

--- src/Components/PlayingField/PlayingField.js
+++ src/Components/PlayingField/PlayingField.js
@@ -6,12 +6,31 @@ import { Card } from '../Card/Card';
 export function PlayingField(props) {
 
     return (
-        <section id="playing-field">
+        <section>
+            <div id="game-info" className='playing-field'>
+                {props.cards.length === 0 ? <p>Please hit 'New Game'</p> : (
+                    <div>
+                        <span>open cards: {openCards.map(card => {
+                            return <span key={card}>{card}, </span>;
+                        })}</span>
+                        <span> ; </span>
+                        <span>number of open cards: {numberOfOpenCards}</span>
+                </div>
+                )}
+            </div>
+            <div id="cards" className='playing-field'>
             {
                 props.cards.map(card => {
-                    return <Card card={card} key={card.id} />
+                        return <Card 
+                            key={card.id}
+                            card={card}
+                            incrementOpenCards={addOpenCard}
+                            resetOpenCards={resetNumberOfOpenCards}
+                            numberOfOpenCards={numberOfOpenCards}
+                        />
                 })
             }
+            </div>
         </section>
     );
 }

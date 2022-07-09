a4769a58ddb0ca05098c2efeab456bdd3467f9f2
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-09 23:51:18 +0200

    Add return statement to MemoryImages

--- src/Components/MemoryImages/MemoryImages.js
+++ src/Components/MemoryImages/MemoryImages.js
@@ -3,6 +3,7 @@ import './MemoryImages.css';
 import { Card } from '../Card/Card';
 
 export function MemoryImages(props) {
+    const [cards, setCards] = useState([]);
 
     function shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
@@ -11,5 +12,23 @@ export function MemoryImages(props) {
         }
         return array;
     }
-    
+
+
+    return (
+        <section id='gameApp'>
+            <div id='stats'>
+                images memory
+            </div>
+            <div id='cards'>
+                {
+                    cards.map(card => {
+                        <Card 
+                            key={card.id}
+                            card={card}
+                        />
+                    })
+                }
+            </div>
+        </section>
+    )
 }

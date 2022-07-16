45dc645c60cdf3baaf9579226b193ecdf57ac112
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-17 01:30:45 +0200

    Transfer JSX from MemoryImages into return statement

--- src/Components/PlayerStats/PlayerStats.js
+++ src/Components/PlayerStats/PlayerStats.js
@@ -3,4 +3,24 @@ import './PlayerStats.css';
 
 export function PlayerStats(props) {
 
+    return (
+        <section className='playerstats'>
+            <div>
+                <input
+                    id={inputId} 
+                    className='hidden' 
+                    type='text'
+                    placeholder={playerName}
+                    value={playerName}
+                    onChange={handleChange}
+                ></input>
+                <h3 id={playerNameId}>{playerName} {props.activePlayer === props.playerId ? 'has the turn.' : ''} </h3>
+                <button title='Edit Player Name' onClick={handleClick}>
+                    <img id={editImageId} className='edit-icon' src={iconEdit} alt='' />
+                    <img id={doneImageId} className='done-icon hidden' src={iconDone} alt='' />
+                </button>
+            </div>
+            <p>Score: {props.score} </p>
+        </section>
+    )
 }

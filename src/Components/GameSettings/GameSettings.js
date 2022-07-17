4926f73311fd25f67b105881cb12d9d6d3ec1b03
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-17 18:50:08 +0200

    Update head of file
    - remove useEffect as not needed
    - fixed import path to css file

--- src/Components/GameSettings/GameSettings.js
+++ src/Components/GameSettings/GameSettings.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './MemoryImages.css';
+import React, { useState } from 'react';
+import './GameSettings.css';
 
 export function GameSettings(props) {
 

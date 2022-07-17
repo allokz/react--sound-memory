6571ede55957504802c8c85e8a0334db62971ea6
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-17 14:40:21 +0200

    Create GameSettings component
    Add new component with JS boilerplate

---
+++ src/Components/GameSettings/GameSettings.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './MemoryImages.css';
+
+export function GameSettings(props) {
+
+
+    return (
+        <section id='game-settings'>
+
+        </section>
+    )
+}

3d28d412f2e27d9762f09b6e41e343116c03f40f
Alexander <84243644+allokz@users.noreply.github.com>
2022-07-18 17:24:07 +0200

    Use player name prop instead of state var

--- src/Components/PlayerStats/PlayerStats.js
+++ src/Components/PlayerStats/PlayerStats.js
@@ -32,11 +32,10 @@ export function PlayerStats(props) { // props: playerId, activePlayer, score
                     id={inputId} 
                     className='hidden' 
                     type='text'
-                    placeholder={playerName}
                     value={playerName}
                     onChange={handleChange}
                 ></input>
-                <h3 id={playerNameId}>{playerName} {props.activePlayer === props.playerId ? 'has the turn.' : ''} </h3>
+                <h3 id={playerNameId}>{props.playerName} {props.activePlayer === props.playerId ? 'has the turn.' : ''} </h3>
                 <button title='Edit Player Name' onClick={handleClick}>
                     <img id={editImageId} className='edit-icon' src={iconEdit} alt='' />
                     <img id={doneImageId} className='done-icon hidden' src={iconDone} alt='' />

cbbeb14b5fb3491d4124de686585e75fba580231
Alexander <84243644+allokz@users.noreply.github.com>
2022-10-16 16:47:36 +0200

    remove header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -40,9 +40,6 @@ function App() {
 
     return (
         <div className="App">
-            <header>
-                <h1>Memory</h1>
-            </header>
             <main>
                 <div id='settings' className=''>
                     <GameSettings

2a2800edd7c9b3b3f14e339d881eaa2f105d3bda
Alexander <84243644+allokz@users.noreply.github.com>
2022-10-17 18:50:45 +0200

    Add JSX to Scoreboard component
    mainly the same content as in old PlayerStats component, adjusted to match new design

--- src/Components/Scoreboard/Scoreboard.js
+++ src/Components/Scoreboard/Scoreboard.js
@@ -4,8 +4,32 @@ import './Scoreboard.css';
 export function Scoreboard(props) {
 
     return (
-        <div>
+        <div className='scoreboard'>
+            <div className='scoreboard-name'>
+                <input
+                    type='text'
+                    className='hidden'
+                ></input>
+                <h3>{props.playerNames.player1}</h3>
+                <button>
+                    <img id='edit-1' className='edit-icon' src={iconEdit} alt='' />
+                    <img id='done-1' className='done-icon hidden' src={iconDone} alt='' />
+                </button>
+            </div>
 
+            <h2>{props.score.player1} : {props.score.player2}</h2>
+            
+            <div className='scoreboard-name'>
+                <input
+                    type='text'
+                    className='hidden'
+                ></input>
+                <h3>{props.playerNames.player2}</h3>
+                <button>
+                    <img id='edit-2' className='edit-icon' src={iconEdit} alt='' />
+                    <img id='done-2' className='done-icon hidden' src={iconDone} alt='' />
+                </button>
+            </div>
         </div>
     )
 }
